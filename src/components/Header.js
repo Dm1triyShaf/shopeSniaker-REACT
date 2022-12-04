6aa2b50fdd39b8f5e9ba70c5f4d497b243da3a5d
Dmitriy Shafalyuk <dima.shafalyuk@mail.ru>
2022-12-04 17:58:13 +0300

    firstcommit

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,20 @@ import React, {useState}  from 'react';
 import { FiShoppingCart } from "react-icons/fi";
 import Orders from './Orders';
 
+const showOrders = (props) => {
+    return (<div>
+        {props.orders.map(el => (
+                      <Orders key={el.id} item= {el} />
+        ))}
+    </div>)
+}
+
+const showNothing = () => {
+  return (<div className='empty'>
+    <h2>Товаров нет</h2>
+  </div>)
+}
+
 
 export default function Header(props) {
   let [cartOpen, setCartOpen] = useState(false)
@@ -19,9 +33,8 @@ export default function Header(props) {
 
               {cartOpen && (
                   <div className='shop-cart'>
-                    {props.orders.map(el => (
-                      <Orders key={el.id} item= {el} />
-                    ))}
+                    {props.orders.length > 0 ?
+                      showOrders(props) : showNothing()}
                   </div>
               )}
         </div>
